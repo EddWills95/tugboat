00da0faacbd97a0b7bc6e195b8c38f9337be304f
Edd Williams <edd.williams@me.com>
2025-07-20 23:58:29 +0100

    rework the auth layouts and signout works now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,29 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :authenticate_user!
+
+  # Use devise layout for authentication pages
+  layout :layout_by_resource
+
+  protected
+
+  # Redirect to projects page after sign in
+  def after_sign_in_path_for(resource)
+    projects_path
+  end
+
+  # Redirect to projects page after sign out
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+  private
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 end
